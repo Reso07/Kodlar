548da243953565ee46862684c2be3fbb439653f4
Reso07 <64981906+Reso07@users.noreply.github.com>
2020-05-10 20:36:18 +0300

    Update Main.java

--- AdventureGame/src/JavaCourse/Main.java
+++ AdventureGame/src/JavaCourse/Main.java
@@ -17,19 +17,57 @@ public class Main {
         locationMap.put(4,new Location(4,"You are in a valley beside a stream"));
         locationMap.put(5, new Location(5,"You are by the sea shore"));
 
-        int loc = 1;
-        while(true) {
-            System.out.println(locationMap.get(loc).getDescription());
-            if(loc == 0) {
-                break;
-            }
-            loc = scanner.nextInt();
-            if(!locationMap.containsKey(loc)) {
-                System.out.println("You cannot go in that direction");
-            }
+        locationMap.get(1).addExit("W",2);
+        locationMap.get(1).addExit("E",3);
+        locationMap.get(1).addExit("S",4);
+        locationMap.get(1).addExit("N",5);
+
+        locationMap.get(2).addExit("N",5);
+
+        locationMap.get(3).addExit("W",1);
+
+        locationMap.get(4).addExit("N",1);
+        locationMap.get(4).addExit("W",2);
+
+        locationMap.get(5).addExit("S",1);
+        locationMap.get(5).addExit("W",2);
+
+
+
+     //   int loc = 1;
+     //   while(true) {
+     //       System.out.println(locationMap.get(loc).getDescription());
+     //       if(loc == 0) {
+     //           break;
+     //       }
+
+     //       Map<String,Integer> exits = locationMap.get(loc).getExits();
+     //       System.out.print("Available exits are ");
+     //       for(String exit : exits.keySet()) {
+     //           System.out.print(exit + " , ");
+     //       }
+     //       System.out.println();
+
+     //       String direction = scanner.nextLine().toUpperCase();
+
+     //       if(exits.containsKey(direction)) {
+     //           loc = exits.get(direction);
+     //       } else {
+     //           System.out.println("You cannot go in that direction");
+     //       }
+     //   }
+
+        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
+        for(String i : road) {
+            System.out.println(i);
         }
 
+        System.out.println("===========================================================");
 
+        String[] building = "You are inside a building, a well house for a small spring".split(", ");
+        for(String i: building) {
+            System.out.println(i);
+        }
 
     }
 }
